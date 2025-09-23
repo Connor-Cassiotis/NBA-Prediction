015d0035666b5ace0f8d74af2c31fb2d18c6ef8d
Connor-Cassiotis <140370054+Connor-Cassiotis@users.noreply.github.com>
2025-09-22 23:28:53 -0400

    Add import and clean data

---
+++ predict.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.feature_selection import SequentialFeatureSelector
+from sklearn.linear_model import RidgeClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('nba_games.csv', index_col=0)
+df = df.sort_values(by='date')
+df = df.reset_index(drop=True)
+
+# Data cleaning
+del df["mp.1"]
+del df["mp_opp.1"]
+del df["index_opp"]
+
+def add_target(team):
+    team = team.copy()
+    team["target"] = team["won"].shift(-1)
+    return team
+
+df = df.groupby("team", group_keys=False).apply(add_target)
+df[df["team"] == "WAS"]
+df.loc[pd.isnull(df["target"]), "target"] = 2
+df["target"] = df["target"].astype(int, errors='ignore')
+nulls = df.isnull().sum()
+nulls = nulls[nulls > 0]
+valid_columns = df.columns[~df.columns.isin(nulls.index)]
+df = df[valid_columns].copy()
+
+# Machine learning
+rr = RidgeClassifier(alpha=1)
+split = TimeSeriesSplit(n_splits=3)
+
+sfs = SequentialFeatureSelector(rr, n_features_to_select=30, direction='forward', cv=split)
+removed_columns = ["season", "date", "team", "target", "won", "team_opp"]
+selected_columns = df.columns[~df.columns.isin(removed_columns)]
+scaler = MinMaxScaler()
+df[selected_columns] = scaler.fit_transform(df[selected_columns])
+sfs.fit(df[selected_columns], df["target"])
+predictors = list(selected_columns[sfs.get_support()])
+
+def backtest(data, model, predictors, start=2, step=1):
+    all_predictions = []
+
+    seasons = sorted(data["season"].unique())
+    for i in range(start, len(seasons), step):
+       season = seasons[i]
+       train = data[data["season"] < season]
+       test = data[data["season"] == season]
+
+       model.fit(train[predictors], train["target"])
+
+       preds = model.predict(test[predictors])
+       preds = pd.Series(preds, index=test.index)
+
+       combined = pd.concat([test["target"], preds], axis=1)
+       combined.columns = ["actual", "prediction"]
+       all_predictions.append(combined)
+    return pd.concat(all_predictions)
+
+predictions = backtest(df, rr, predictors)
+predictions = predictions[predictions["target"] != 2]
+accuracy = accuracy_score(predictions["actual"], predictions["prediction"])
+
